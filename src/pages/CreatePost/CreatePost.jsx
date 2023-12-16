53091b3b0f5cdc91a0648261d0400e382a8fd7b1
Marka <khalilovakram07@gmail.com>
2023-12-16 14:59:41 +0300

    added post http requesrt, added snackbar with hepl material ui and operator no

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,36 +1,85 @@
 import React, { useState } from "react"; // импорт useState
+import axios from "axios";
+
 import "./createpost.css";
 
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
+import { Snackbar } from "@mui/material";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
+import { useNavigate } from "react-router-dom";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 const CreatePost = () => {
+  const [title, setTitle] = useState(""); // setTitle - это функция которая позволяет изменить состояние
+  const [duration, setDuration] = useState(0); // состояние
+  const [open, setOpen] = useState(false); // отображает нам уведомление
+  const navigate = useNavigate();
 
-    const [title, setTitle] = useState("")
-    const [duration, setDuration] = useState(0)
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-    
+    setOpen(false);
+  };
+
+  const toHome = () =>
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
 
   const addPost = (e) => {
-    e.preventDefault()
-    console.log("Sended");
+    e.preventDefault(); // это нужно для отмены обновления страницы
+
+    const newData = {
+      // это наши новые данные, которые нужно отправить
+      title,
+      duration,
+    };
+
+    axios
+      .post("http://localhost:3001/courses", newData) // promise
+      .then((res) => {
+        setTitle("");
+        setDuration(0);
+        setOpen(true);
+        toHome();
+      }); // обработка promise
   };
 
   return (
     <div className="form-wrapper">
-      <form onSubmit={addPost}> 
-      {/* onSubmit - он срабатывает лишь при отправке */}
+      <form onSubmit={addPost}>
+        {/* onSubmit - он срабатывает лишь при отправке */}
         <h1>CREATE POST FORM</h1>
-        <TextField id="standard-basic" label="Title..." variant="standard" value={title} 
-        onChange={(e) => setTitle(e.target.value)} />
+        <TextField
+          id="standard-basic"
+          label="Title..."
+          variant="standard"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)} // onChange() - вызывает событие, это событие мы можем получить
+        />
         {/* e.target.value - данные которые я ввожу в инпут */}
-        <TextField id="standard-basic" label="Duration..." variant="standard" value={duration}
-            onChange={(e) => setDuration(e.target.value)}
+        <TextField
+          id="standard-basic"
+          label="Duration..."
+          variant="standard"
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
         />
         <Button variant="contained" color="success" type="submit">
           SEND
         </Button>
       </form>
+      <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          This is post sended!
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -7,35 +7,46 @@ import axios from "axios";
 // и сделать для неё роут
 
 const getData = () => {
-  return axios("http://localhost:3001/courses")
-}
+  return axios("http://localhost:3001/courses");
+};
 
 const HomePage = () => {
 
   const [courses, setCourses] = useState([]); // состояние нужно для получение данных с ответа
+  const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    getData()
-      .then((res) => setCourses(res.data))
+    getData().then((res) => setCourses(res.data));
     // promise = {resolve, reject}
   }, []); // пустой массив в виде второго параметра, позволяет выполнить функцию одина раза
 
-  console.log(courses);
+  // менять состояние showAll(false), но если он уже false то менять его на true
+  // при нажатии на кнопку
+  // ! - не
+
+  const changeState = () => {
+    setShowAll(!showAll) // !true = false, !false = true
+  }
 
   // useEffect() - позволяет выполнить какой-то эффект на странице
 
   // console.log(courses);
 
   return (
-    <div className="course-wrapper">
+    <div className="wrapper">
       {/* 
         использовать метод перебора массива (map) и отобразить три карточки
         с информацией о курсах
     */}
 
-      {courses.map((course) => (
-        <CourseItem key={course.id} course={course} />
-      ))}
+      <button onClick={changeState}>Show Important Courses</button>
+
+      <div className="course-wrapper">
+        {courses.map((course) => (
+          <CourseItem key={course.id} course={course} />
+        ))}
+      </div>
+
     </div>
   );
 };
